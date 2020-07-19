385532e7601303b675053bed64c68ff3d3c762c8
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2020-07-19 10:18:58 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+	// Mobile Menu
+	$('.nav-bars').click(function(){
+		$('.main-menu ul').slideToggle(1000);
+	});
+	// Counter Active
+	$('.single-counter h1').counterUp({
+		delay: 5,
+		time: 1400
+	});
+	$(window).on("load",function(){
+        $(".accor_content p").mCustomScrollbar();
+    });
+	
+	// Client Carousel
+	$('.single-client').owlCarousel({
+		loop:true,
+		items:1,
+		autoplay:true,
+		nav:true,
+		navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>']
+		
+	});
+	// Client Carousel
+	$('.single-client-img').owlCarousel({
+		loop:true,
+		autoplay:true,
+		margin:20,
+		responsive :{
+			0:{
+				items:1
+			},
+			767:{
+				items:3
+			},
+			990:{
+				items:6
+			}
+		}
+	});
+	
+	// Magnific PopUp
+
+	$('.card-overly a').magnificPopup({
+	  type: 'image',
+	  gallery:{
+	    enabled:true
+	  }
+	});
+	// Google Map JS
+
+	function basicmap() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 11,
+            scrollwheel: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(25.6503578,88.7060858), // New York
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": .2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
+        };
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('contact-map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(25.6503578,88.7060858),
+            map: map,
+            title: 'Cryptox'
+        });
+    }
+    if ($('#contact-map').length != 0) {
+        google.maps.event.addDomListener(window, 'load', basicmap);
+    }
+
+
+});
